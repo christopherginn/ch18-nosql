71c425b874b41c90565d6653cc3db4b2b9f290c4
christopherginn <hey.im.chris@live.com>
2022-07-02 11:11:43 -0400

    made thought model'

---
+++ Main/Main/models/Thought.js
@@ -0,0 +1,38 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction');
+
+const thoughtSchema = new Schema(
+    {
+      thoughtText: {
+        type: String,
+        required: true,
+        maxLength: 280,
+        minLength: 1,
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+        // needs getter to format time stamp
+      },
+      username: {
+        type: String,
+        required: true,
+      },
+      reactions: [reactionSchema],
+    },
+    {
+      toJSON: {
+        getters: true,
+        virtuals: true,
+      },
+    }
+  );
+  thoughtSchema
+  .virtual('reactionCount')
+  .get(function () {
+    return this.reactions.length;
+  });
+  
+  const User = model('thought', thoughtSchema);
+  
+  module.exports = Thought;
--- Main/Main/models/User.js
+++ Main/Main/models/User.js
@@ -30,10 +30,17 @@ const userSchema = new Schema(
     {
       toJSON: {
         getters: true,
+        virtuals: true,
       },
     }
   );
+
+  userSchema
+  .virtual('friendCount')
+  .get(function () {
+    return this.friends.length;
+  });
   
-  const User = model('friend', userSchema);
+  const User = model('user', userSchema);
   
   module.exports = User;
